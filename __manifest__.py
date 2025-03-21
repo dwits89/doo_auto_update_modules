14315b96797a7c1bd269a2f1a28951e88e971561
DWITS <lobo.digital.web@gmail.com>
2025-03-21 10:53:12 +0100

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Auto Update Modules',
+    'version': '16.0.1.0.0',
+    'summary': 'Actualiza automáticamente la lista de módulos al crear una base de datos',
+    'category': 'Tools',
+    'author': 'Tu Empresa',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
